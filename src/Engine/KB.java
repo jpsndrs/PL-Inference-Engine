d3841697325fa29d6ea260871952b8845a869e0e
Thiago Sandeiro <102810162@student.swin.edu.au>
2022-05-18 19:19:31 +1000

    Update KB.java

--- src/Engine/KB.java
+++ src/Engine/KB.java
@@ -12,11 +12,31 @@ import java.util.List;
  * */
 public class KB {
 
-	private List<String> fClause = new ArrayList<>();
+	private List<HornClause> fKB;
+	
+	KB() {
+		fKB = new ArrayList<>();
+	}
 
 	public void tell(String aSentence) throws IOException {
 		
-		String[] lClause = aSentence.split(";");
+		//string array to temporarily hold each horn clause
+		String[] lData = aSentence.split(";");
+		//string array to temporarily hold each side of horn clause.
+		String[] lHornClause;
+		//loop over each horn clause.
+		for(String dataElement:lData) {
+			//check length, if it's not bigger than two, if true it has no body.
+			//implication only.
+			if(dataElement.length() > 2) {
+				lHornClause = dataElement.split("=>");
+				
+			}else {
+				//else set to head.
+			}
+		}
+		
+		/*String[] lClause = aSentence.split(";");
 		for(String subStr:lClause) {
 			fClause.add(subStr.strip());
 			//debug
@@ -24,7 +44,7 @@ public class KB {
 			System.out.println(subStr.strip());
 			System.out.println("");
 			//debug end
-		}
+		}*/
 	}
 	
 	
