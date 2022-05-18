61bfe19d1832a25d685e0e42a270f598d9667f75
Thiago Sandeiro <102810162@student.swin.edu.au>
2022-05-18 20:29:03 +1000

    Create PropositionCollector.java
    Co-Authored-By: rjunderwood <29243316+rjunderwood@users.noreply.github.com>

---
+++ src/Engine/PropositionCollector.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package Engine;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * @author James Sanders
+ * @author Riley Underwood
+ */
+public class PropositionCollector {	
+	
+	/**
+	 * Collect a set of propositional symbols from a list of given sentences.
+	 * 
+	 * @param sentences
+	 *            a list of sentences from which to collect symbols.
+	 * @return a set of all the proposition symbols that are not always true or
+	 *         false contained within the input sentences.
+	 */
+	public static Set<String> getSymbolsFrom(KB aKB, String aAlpha){
+		Set<String> result = new LinkedHashSet<>();
+	}
+
+}
